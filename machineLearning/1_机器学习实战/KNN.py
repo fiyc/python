da7b968b9e95878a8984b6712055b996e97f3cad
fiyc <626897681@qq.com>
2018-02-23 00:07:50 +0800

    机器学习k近邻算法

---
+++ machineLearning/1_机器学习实战/KNN.py
@@ -0,0 +1,34 @@
+#-*- coding: utf-8 -*-
+from numpy import *
+
+'''
+这是《机器学习实战》这本书中, k近邻算法的实现
+
+'''
+
+'''
+intX: 用于分类的输入向量
+dataSet: 输入的训练样本集
+labels: 标签向量(元素数目和dataSet的行数相同)
+k: 选择最近邻居的数目
+'''
+def classify(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+
+    # 计算距离
+    diffMat = tile(intX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    
+    distances = sqDistances**0.5
+    sortedDisIndicies = distances.argsort()
+    classCount={}
+
+    # 选择距离最小的k个点
+    for i in range(k):
+        votellabel = labels[sortedDisIndicies[i]]
+        classCount[votellabel] = classCount.get(votellabel, 0) + 1
+
+    sortedClassCount = sorted(classCount.iteritems(), key = operator.itemgetter(1), reverse = True)
+
+    return sortedClassCount[0][0]
