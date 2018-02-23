c77b36cc513cd4343659f8ffa0dba4c586e7ec63
fiyc <626897681@qq.com>
2018-02-24 00:30:58 +0800

    K-近邻算法完整式例-1

--- machineLearning/1_机器学习实战/KNN.py
+++ machineLearning/1_机器学习实战/KNN.py
@@ -32,3 +32,61 @@ def classify(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.iteritems(), key = operator.itemgetter(1), reverse = True)
 
     return sortedClassCount[0][0]
+
+'''
+将文本记录转换到NumPy的解析程序
+'''
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros(numberOfLines, 3)
+    classLabelVector = []
+
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+
+    return returnMat, classLabelVector
+
+'''
+归一化特征值
+newValue = (oldValue - min) / (max - min)
+'''
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet / tile(ranges, (m, 1))
+
+    return normDataSet, ranges, minVals
+
+'''
+分类器针对约会网站的测试代码
+'''
+def datingClassTest():
+    hoRatio = 0.1
+    datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+    norMat, ranges, minVals = autoNorm(datingDataMat)
+    m = norMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+
+    errorCount = 0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+
+        print "the classifier came back with %d, the real answer is: %d" % (classifierResult, datingLabels[i])
+
+        if(classifierResult != datingLabels[i]):
+            errorCount += 1
+
+    print "the total error rate is: %f" % (errorCount / float(numTestVecs))
